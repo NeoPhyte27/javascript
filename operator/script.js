dc841b787d55ccd2189c714833040dcf62cb0a6d
NeoPhyte27 <aldijinkpro27@gmail.com>
2024-11-16 10:16:35 +0700

    operator

---
+++ operator/script.js
@@ -0,0 +1,16 @@
+// aritmatika
+/**
+    10 / 5 = 2
+    10 % 5 = 0 ( sisa dari pembagian )
+    10 % 3 = 1 ( karena 3*3 hasil nya 9 dan ada sisa 1 yaitu angka 10)
+
+    hati-hati jika melakukan aritmatika secara bersamaan
+    misal: 1+2*3-4 = 3( karena ada urusan operasi matematika)
+
+operator precedence
+    urutan operasi matematika
+KuKaBaTaKu = singkatan dari
+kurung kali bagi tambah kurang
+    ()        *      /         +           -
+yang akan dikerjakan duluan dari kiri ke kanan
+ */
